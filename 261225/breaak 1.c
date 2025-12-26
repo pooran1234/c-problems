9f1ac64c9e7dcf1f9311e489d8c83a132e4d75b9
pooran1234 <rajpurohitpooran68@gmail.com>
2025-12-26 19:53:17 +0530

    Add files via upload

---
+++ 261225/breaak 1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+    float a, b;
+
+    printf("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &a, &b);
+
+    switch(choice) {
+        case 1:
+            printf("Sum = %.2f", a + b);
+            break;
+        case 2:
+            printf("Difference = %.2f", a - b);
+            break;
+        case 3:
+            printf("Product = %.2f", a * b);
+            break;
+        case 4:
+            if(b != 0)
+                printf("Division = %.2f", a / b);
+            else
+                printf("Cannot divide by zero");
+            break;
+        default:
+            printf("Invalid choice");
+    }
+    return 0;
+}
+
