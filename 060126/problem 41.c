835e8e3d48e6e1018f947f5fdc98840f61c82d5c
pooran1234 <rajpurohitpooran68@gmail.com>
2026-01-06 21:14:10 +0530

    Add files via upload

---
+++ 060126/problem 41.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+
+    scanf("%d", &n);
+    int a[n];
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+        if(a[i] < 0) {
+            count++;
+        }
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
