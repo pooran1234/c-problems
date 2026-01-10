6036c43bfdc7703fd6db90b9c0a478289220ac63
pooran1234 <rajpurohitpooran68@gmail.com>
2026-01-10 22:23:04 +0530

    Add files via upload

---
+++ 100126/problem 51.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    int i, count = 0;
+
+    gets(str);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        char ch = tolower(str[i]);
+        if(ch >= 'a' && ch <= 'z') {
+            if(ch!='a' && ch!='e' && ch!='i' && ch!='o' && ch!='u')
+                count++;
+        }
+    }
+
+    printf("%d", count);
+    return 0;
+}
